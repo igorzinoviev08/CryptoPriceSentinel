4751344173c50572957e71f6916ca0c4e2e1acb5
igor <zinovievigor@gmail.com>
2024-01-30 00:53:47 +0300

    Testing the check_price_movement function

---
+++ tests/test_price_alert.py
@@ -0,0 +1,8 @@
+from src.price_alert import check_price_movement
+
+
+def test_check_price_movement():
+    # Мокирование модели и входных данных
+    mock_model = None  # Замените на подходящий мок объекта модели
+    check_price_movement(mock_model)
+    # Проверьте ожидаемое поведение функции
